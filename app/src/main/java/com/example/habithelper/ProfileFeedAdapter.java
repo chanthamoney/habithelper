07836e416cd4c4d469d7d49302bdcbd7cfa73e12
cassandrachanthamontry <chant077@umn.edu>
2019-04-06 18:13:14 -0500

    Added Profile Page

---
+++ app/src/main/java/com/example/habithelper/ProfileFeedAdapter.java
@@ -0,0 +1,66 @@
+package com.example.habithelper;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.List;
+
+public class ProfileFeedAdapter extends RecyclerView.Adapter<ProfileFeedAdapter.ProfileFeedViewHolder> {
+    //this context we will use to inflate the layout
+    private Context mCtx;
+
+    //we are storing all the products in a list
+    private List<ProfileFeed> profileFeedList;
+
+    //getting the context and product list with constructor
+    public ProfileFeedAdapter(Context mCtx, List<ProfileFeed> profileFeedList) {
+        this.mCtx = mCtx;
+        this.profileFeedList = profileFeedList;
+    }
+
+    @Override
+    public ProfileFeedViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //inflating and returning our view holder
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.layout_profile_feed, null);
+        return new ProfileFeedViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ProfileFeedViewHolder holder, int position) {
+        //getting the product of the specified position
+        ProfileFeed profileFeed = profileFeedList.get(position);
+
+        //binding the data with the viewholder views
+        holder.textViewTitle.setText(profileFeed.getTitle());
+        holder.imageView.setImageDrawable(mCtx.getResources().getDrawable(profileFeed.getImage()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return profileFeedList.size();
+    }
+
+    class ProfileFeedViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewTitle;
+        ImageView imageView;
+
+        public ProfileFeedViewHolder(View itemView) {
+            super(itemView);
+
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+
+}
+
+
+
+
+
---
+++ app/src/main/java/com/example/habithelper/Profile_Page.java
@@ -0,0 +1,148 @@
+package com.example.habithelper;
+
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Profile_Page extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    //a list to store all the Profile Feed Posts
+    List<ProfileFeed> profileFeedList;
+
+    //the recyclerview
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile__page);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //getting the recyclerview from xml
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //initializing the productlist
+        profileFeedList = new ArrayList<>();
+        //adding some items to our list
+        profileFeedList.add(
+                new ProfileFeed(1, "Kassi washed her hands",
+                        R.drawable.profilepikture));
+
+        profileFeedList.add(
+                new ProfileFeed(1, "Kassi ate an apple",
+                        R.drawable.profilepikture));
+        profileFeedList.add(
+                new ProfileFeed(1, "Kassi watched her lecture video",
+                        R.drawable.profilepikture));
+        profileFeedList.add(
+                new ProfileFeed(1, "Kassi recycled her box of snacks",
+                        R.drawable.profilepikture));
+
+        //creating recyclerview adapter
+        ProfileFeedAdapter adapter = new ProfileFeedAdapter(this, profileFeedList);
+
+        //setting adapter to recyclerview
+        recyclerView.setAdapter(adapter);
+
+    }
+
+
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.content_profile__page);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+//
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
+//    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.profile__page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}

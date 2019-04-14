ca2ae0dcb3ef5e4c5805b3a12f897a94b9e81f71
Jake Nippert <nippe014@umn.edu>
2019-04-14 14:41:40 -0500

    oops please god help us

--- app/src/main/java/com/example/habithelper/SettingsActivity.java
+++ app/src/main/java/com/example/habithelper/SettingsActivity.java
@@ -151,6 +151,16 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         return isXLargeTablet(this);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     /**
      * {@inheritDoc}
      */
